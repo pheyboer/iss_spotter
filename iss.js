5d091950d7664d1fd5bfad301d02d8ece222d9ac
Peter Heyboer <heyboer.peter@gmail.com>
2024-10-25 15:33:59 -0400

    added fetchCoordsByIP function

--- iss.js
+++ iss.js
@@ -5,7 +5,15 @@
  * Returns (via Callback):
  *   - An error, if any (nullable)
  *   - The IP address as a string (null if error). Example: "162.245.144.188"
+ * 
  */
+// IPv4
+// $ curl 'https://api.ipify.org?format=json'
+
+
+// Find IP in JSON Format
+// https://api.ipify.org?format=json
+
 
 const needle = require('needle');
 
@@ -25,12 +33,27 @@ const fetchMyIP = function(callback) {
   });
 };
 
-module.exports = { fetchMyIP };
+const fetchCoordsByIP = function (ip, callback) {
+  needle.get(`http://ipwho.is/${ip}`, (error, response, body) => {
 
+    if (error) {
+      callback(error, null);
+      return;
+    }
 
-// //IPv4
-// $ curl 'https://api.ipify.org?format=json'
+    if (!body.success) {
+      const message = `Success status was ${body.success}. Server Message says: ${body.message} when fetching for IP ${body.ip}`;
+      callback(Error(message), null);
+      return;
+    }
 
+    const latitude = body.latitude
+    const longitude = body.longitude
+    callback(null, {latitude, longitude});
+  });
+};
 
-// // Find IP in JSON Format
-// https://api.ipify.org?format=json
+module.exports = { 
+  fetchMyIP,
+  fetchCoordsByIP
+};
