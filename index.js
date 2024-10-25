738787f064989d46e8bdf77986bff014e3e5431c
Peter Heyboer <heyboer.peter@gmail.com>
2024-10-25 15:50:48 -0400

    added fetchISSFlyOverTimes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const { fetchMyIP } = require('./iss');
 const { fetchCoordsByIP } = require('./iss');
+const { fetchISSFlyOverTimes } = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -15,9 +16,21 @@ fetchMyIP((error, ip) => {
 
 fetchCoordsByIP('99.227.221.244', (error, coordinates) => {
   if (error) {
-  console.log('It did not work!', error);
-  return;
+    console.log('It did not work!', error);
+    return;
   }
 
   console.log('It worked! Return Coordinates:', coordinates);
+});
+
+
+const exampleCoords = { latitude: '43.653226', longitude: '-79.3831843' };
+
+fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
+  if (error) {
+    console.log("It didn't work!", error);
+    return;
+  }
+
+  console.log('It worked! Return flyover times:', passTimes);
 });
