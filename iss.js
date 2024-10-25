357bf0cf386b0e45b13cc94d9964dadadefd46ff
Peter Heyboer <heyboer.peter@gmail.com>
2024-10-25 13:40:50 -0400

    updating iss.js

--- iss.js
+++ iss.js
@@ -12,7 +12,7 @@ const needle = require('needle');
 const fetchMyIP = function(callback) {
   needle.get('https://api.ipify.org?format=json', (error, response, body) => {
     if (error) {
-      return callback(error);
+      return callback(error, null);
     }
 
     if (response.statusCode !== 200) {
