b9693a943a34295674339a77cf2e5acd3d4536ab
Peter Heyboer <heyboer.peter@gmail.com>
2024-10-25 13:35:47 -0400

    updating iss.js

---
+++ iss.js
@@ -0,0 +1,36 @@
+/**
+ * Makes a single API request to retrieve the user's IP address.
+ * Input:
+ *   - A callback (to pass back an error or the IP string)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The IP address as a string (null if error). Example: "162.245.144.188"
+ */
+
+const needle = require('needle');
+
+const fetchMyIP = function(callback) {
+  needle.get('https://api.ipify.org?format=json', (error, response, body) => {
+    if (error) {
+      return callback(error);
+    }
+
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching IP: ${body}`), null);
+      return;
+    }
+
+    const ip = body.ip;
+    callback(null, ip);
+  });
+};
+
+module.exports = { fetchMyIP };
+
+
+// //IPv4
+// $ curl 'https://api.ipify.org?format=json'
+
+
+// // Find IP in JSON Format
+// https://api.ipify.org?format=json
