1a57fcf5f448c5674558630b8657e6ed2be723bf
Peter Heyboer <heyboer.peter@gmail.com>
2024-10-25 15:33:42 -0400

    updating index.js with fetchCoordsByIP

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Index.js
 
 const { fetchMyIP } = require('./iss');
+const { fetchCoordsByIP } = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -9,4 +10,14 @@ fetchMyIP((error, ip) => {
   }
 
   console.log('It worked! Returned IP:' , ip);
+});
+
+
+fetchCoordsByIP('99.227.221.244', (error, coordinates) => {
+  if (error) {
+  console.log('It did not work!', error);
+  return;
+  }
+
+  console.log('It worked! Return Coordinates:', coordinates);
 });
