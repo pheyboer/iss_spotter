c2e30acef06072d4c0fa91bc8a0ed66bc05d5ea7
Peter Heyboer <heyboer.peter@gmail.com>
2024-10-28 12:06:35 -0400

    updating index2 with promises

--- index2.js
+++ index2.js
@@ -1,6 +1,26 @@
-const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss_promised');
+const { nextISSTimesForMyLocation } = require('./iss_promised');
+//const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss_promised');
 
-fetchMyIP()
-  .then((ip) => fetchCoordsByIP(ip))
-  .then((coords) => fetchISSFlyOverTimes(coords))
-  .then((body) => console.log(body));
+// fetchMyIP()
+//   .then((ip) => fetchCoordsByIP(ip))
+//   .then((coords) => fetchISSFlyOverTimes(coords))
+//   .then((body) => console.log(body));
+
+const printPassTime = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+// Call
+
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTime(passTimes);
+  })
+  .catch((error) => {
+    console.log("It didn't work: ", error.message);
+  });
